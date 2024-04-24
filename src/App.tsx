4a2a4bceea5a8268d4b809b66bccd46c47212f46
Kelthon <kelthonbalbino@gmail.com>
2024-04-24 13:42:21 -0300

    feat: added FeatureBox and Categories components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Banner from './components/Banner';
+import FeatureBox from './components/FeatureBox';
+import Categories from './components/Categories';
 
 export default function App(): JSX.Element {
   return (
     <>
       <Navbar />
       <Banner />
+      <Categories />
+      <FeatureBox />
       <Footer />
     </>
   );
