e355266ce5a0138e4d60238906980864f76a2f62
Kelthon <kelthonbalbino@gmail.com>
2024-04-26 19:16:23 -0300

    feat: added link hook, removed duplicated code

--- src/components/SelectedProducts.tsx
+++ src/components/SelectedProducts.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import './SelectedProducts.css';
 import React, { useEffect, useState } from 'react';
 import ProductItem, { ProductItemProps as Product } from './ProductItem';
+import { Link } from 'react-router-dom';
 
 function getListItemFromProducts(product: Product): JSX.Element {
   return (
-    <>
     <li key={product.id}>
       <ProductItem
         id={product.id}
@@ -16,24 +16,10 @@ function getListItemFromProducts(product: Product): JSX.Element {
         price={product.price}
         discountPrice={product.discountPrice}
         discountPercent={product.discountPercent}
-        />
+      />
     </li>
-    <li key={product.id}>
-      <ProductItem
-        id={product.id}
-        name={product.name}
-        description={product.description}
-        isNew={product.isNew}
-        imagePath={product.imagePath}
-        price={product.price}
-        discountPrice={product.discountPrice}
-        discountPercent={product.discountPercent}
-        />
-    </li>
-        </>
   );
 }
-
 
 export default function SelectedProducts(): JSX.Element {
   const [products, setProducts] = useState<Product[] | null>(null);
@@ -59,7 +45,7 @@ export default function SelectedProducts(): JSX.Element {
       <h2 className="section-title">Ours Products</h2>
       <ul className="products">{productsList}</ul>
       <p>
-        <a href="#">See more</a>
+        <Link to="/shop">See more</Link>
       </p>
     </section>
   );
