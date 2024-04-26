74a954754d181f93438568d4ed4a89adade9e2e1
Kelthon <kelthonbalbino@gmail.com>
2024-04-26 15:21:37 -0300

    feat: added our products section

---
+++ src/components/SelectedProducts.tsx
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import './SelectedProducts.css';
+import React, { useEffect, useState } from 'react';
+import ProductItem, { ProductItemProps as Product } from './ProductItem';
+
+function getListItemFromProducts(product: Product): JSX.Element {
+  return (
+    <>
+    <li key={product.id}>
+      <ProductItem
+        id={product.id}
+        name={product.name}
+        description={product.description}
+        isNew={product.isNew}
+        imagePath={product.imagePath}
+        price={product.price}
+        discountPrice={product.discountPrice}
+        discountPercent={product.discountPercent}
+        />
+    </li>
+    <li key={product.id}>
+      <ProductItem
+        id={product.id}
+        name={product.name}
+        description={product.description}
+        isNew={product.isNew}
+        imagePath={product.imagePath}
+        price={product.price}
+        discountPrice={product.discountPrice}
+        discountPercent={product.discountPercent}
+        />
+    </li>
+        </>
+  );
+}
+
+
+export default function SelectedProducts(): JSX.Element {
+  const [products, setProducts] = useState<Product[] | null>(null);
+
+  useEffect(() => {
+    if (products === null) {
+      const fetchProducts = async () => {
+        await axios
+          .get<Product[]>('http://localhost:3000/product/all')
+          .then((response) => setProducts(response.data));
+      };
+
+      fetchProducts();
+    }
+  }, [products]);
+
+  const productsList = products?.map((product) =>
+    getListItemFromProducts(product),
+  );
+
+  return (
+    <section id="selected-products">
+      <h2 className="section-title">Ours Products</h2>
+      <ul className="products">{productsList}</ul>
+      <p>
+        <a href="#">See more</a>
+      </p>
+    </section>
+  );
+}
