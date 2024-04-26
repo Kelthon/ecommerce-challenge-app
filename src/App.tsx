253e740e95d83c6c855988559e5ef780b962370b
Kelthon <kelthonbalbino@gmail.com>
2024-04-26 15:37:44 -0300

    refact: removed styles and was added HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import './App.css';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Banner from './components/Banner';
-import FeatureBox from './components/FeatureBox';
-import Categories from './components/Categories';
+import HomePage from './pages/HomePage';
+
 
 export default function App(): JSX.Element {
-  return (
-    <>
-      <Navbar />
-      <Banner />
-      <Categories />
-      <FeatureBox />
-      <Footer />
-    </>
-  );
+  return <HomePage />
 }
